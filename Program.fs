3a0b9af12f8efdad509b7ae237317696596ebb02
Denis <letto-l@yandex.ru>
2019-10-09 23:31:53 +0300

    started

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open MiniSuave.Http
+open MiniSuave.Console
+open MiniSuave.Successful
+
+[<EntryPoint>]
+let main argv =
+  let request = {Route = ""; Type = GET}
+  let response = {Content = ""; StatusCode = 200}
+  let context = {Request = request; Response = response}
+  
+  executeInLoop context (OK "Hello Suave")
+  
+  0
