a119be606103476c8538cd0131a2eac0447dda99
Denis <letto-l@yandex.ru>
2019-10-10 21:26:12 +0300

    choose webparts

--- Program.fs
+++ Program.fs
@@ -1,13 +1,25 @@
-﻿open MiniSuave.Http
+﻿open MiniSuave
+open MiniSuave.Http
 open MiniSuave.Console
 open MiniSuave.Successful
+open MiniSuave.Filters
+open MiniSuave.Combinators
 
 [<EntryPoint>]
 let main argv =
-  let request = {Route = ""; Type = GET}
+  let request = {Route = ""; Type = Http.GET}
   let response = {Content = ""; StatusCode = 200}
   let context = {Request = request; Response = response}
   
-  executeInLoop context (OK "Hello Suave")
+  let app = Choose [
+    GET >=> Path "/hello" >=> OK "Hello GET"
+    POST >=> Path "/hello" >=> OK "Hello POST"
+    Path "/foo" >=> Choose [
+      GET >=> OK "Foo GET"
+      POST >=> OK "FOO POST"
+    ]
+  ]
+  
+  executeInLoop context app
   
   0
--- Suave.fs
+++ Suave.fs
@@ -62,3 +62,41 @@ module Console =
       with
         | ex ->
           printfn "Error : %s" ex.Message
+          
+module Combinators =
+  
+  let compose first second context =
+    async {
+      let! firstContext = first context
+      match firstContext with
+      | None -> return None
+      | Some context ->
+        let! secondContext = second context
+        return secondContext
+    }
+    
+  let (>=>) = compose
+  
+module Filters =
+  open Http
+  
+  let iff condition context =
+    if condition context then
+      context |> Some |> async.Return
+    else
+      None |> async.Return
+      
+  let GET = iff (fun context -> context.Request.Type = GET)
+  let POST = iff (fun context -> context.Request.Type = POST)
+  let Path path = iff (fun context -> context.Request.Route = path)
+  
+  let rec Choose webparts context =
+    async {
+      match webparts with
+      | [] -> return None
+      | x :: xs ->
+        let! result = x context
+        match result with
+        | Some x -> return Some x
+        | None -> return! Choose xs context
+    }
